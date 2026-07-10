a1800f88bcf2abc0360805e841b42d96651e7c65
ahmadsaif12 <ahmads87237@gmail.com>
2026-07-10 11:00:03 +0545

    fix: update LinkedIn URL

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -79,7 +79,7 @@ function Home() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/saifahmad/"
+                  href="https://www.linkedin.com/in/saif-ahmad-32b202241"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
